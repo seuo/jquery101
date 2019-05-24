7065c98c2c1207c44da6042e7666dd78bb0ee09f
Sam Robertson <samrob.nz@gmail.com>
2019-05-24 15:08:39 +1200

    jquery update

--- Jquery101_slider_accordion/js/script.js
+++ Jquery101_slider_accordion/js/script.js
@@ -1,6 +1,5 @@
-$(
-    function(){
-
+$(function(){
+    //-------- slider
     var isOpen = 'false';
 
         $('#slide-button').on('click',function(){
@@ -22,4 +21,33 @@ $(
                     isOpen = 'false';
                 }
        });
-    });
+
+    //-------- accordian
+
+
+
+    $('#accordion p').hide();
+       $('#accordion h2').on('click',function(){
+           var sState = $(this.data('state');
+
+            if (sState == 'close') {
+                $(this).next().slideDown(function(){
+                    $(this).prev().find('i').removeClass('fa-chevron-circle-down').addClass('fa-chevron-circle-up');
+                    //update state
+                });
+                    $(this).data('state','open');
+            }
+            else {
+                $(this).next().slideUp(function(){
+                    $(this).prev().find('i').removeClass('fa-chevron-circle-up').addClass('fa-chevron-circle-down');
+                });
+                $(this).data('state','close');
+                }
+
+
+        });
+
+});
+
+
+    
